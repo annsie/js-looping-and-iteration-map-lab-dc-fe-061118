0179419b46072a7ca2e551b40853956aabe2d40f
Ann Siebecker <asiebecker@hotmail.com>
2018-08-08 23:18:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ function nameToAttributes(drivers){
   return {firstName: splitName[0], lastName: splitName[1]}
 })
 }
+
+
+
+
+
+
+
+
 function attributestoPhrase(drivers) {
   return drivers.map(function(driver){
   return driver.name + 'is from ' + driver.hometown
