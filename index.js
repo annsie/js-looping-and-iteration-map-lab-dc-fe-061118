d6b2e5015b9b9042164d170b1f345469f92e046c
Ann Siebecker <asiebecker@hotmail.com>
2018-08-08 22:59:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function toLower(driver) {
 function lowerCaseDrivers(drivers){
   return drivers.map(toLower)
 }
-function nameToAttributes(drivers) {
-  return drivers.map(function (driver))
+function nameToAttributes(drivers){
+  return drivers.map(function (driver){
   splitName = driver.split('')
   return {firstName: splitName[0], lastName: splitName[1]}
-}
+})
 function attributestoPhrase(drivers) {
   return drivers.map(function(driver))
   return driver.name + 'is from ' + driver.hometown
