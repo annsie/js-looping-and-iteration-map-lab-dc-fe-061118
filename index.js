a6e469f291acf8a7504095a19cc602a4862a8a49
Ann Siebecker <asiebecker@hotmail.com>
2018-08-08 22:48:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file.
+function toLower(driver) {
+  return driver.toLowerCase()
+}
+function lowerCaseDrivers(drivers){
+  return drivers.map(toLower)
+}
+function nameToAttributes(drivers) {
+  splitName = driver.split('')
+  return {firstName: splitName [0], lastName: splitName[1]}
+}
